53b3d1e89c509ee424ff1ff76d6248f0ea3118c8
Ahana Nandi <ahananondi6@gmail.com>
2023-11-10 17:33:45 +0600

    add category

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle';
+import OneCate from './OneCate';
 
 const Category = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
     return (
         <div className='w-10/12 mx-auto text-center my-20'>
             <SectionTitle heading="Browse By Category" subheading = "Pick A Tour Type"></SectionTitle>
+            <div className="grid md:grid-cols-6 grid-cols-2">
+                {
+                    categories.map(category => <OneCate key = {category._id} 
+                        category ={category}></OneCate>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/pages/Home/Category/OneCate.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const OneCate = ({_id, name, image, duration}) => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+            <h4>{name}</h4>
+            <p>{duration} hour</p>
+        </div>
+    );
+};
+
+export default OneCate;
